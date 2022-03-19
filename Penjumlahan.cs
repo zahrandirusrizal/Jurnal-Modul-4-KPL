d3de808068749424425ee05e0e16f7c16a8b367f
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-03-19 09:47:47 +0700

    Commit Pertama

---
+++ Penjumlahan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mod4
+{
+    public class Penjumlahan
+    {
+        public static void SapaUser<T>(T inputUser)
+        {
+            Console.WriteLine("Halo " + inputUser);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mod4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
