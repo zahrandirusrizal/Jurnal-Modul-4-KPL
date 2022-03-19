6d2592e1f9b8ea9679b2643a7899145673489339
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-03-19 10:27:19 +0700

    Menambahkan Class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace mod4
         static void Main(string[] args)
         {
             Penjumlahan.JumlahTigaAngka<long, long, long>(13, 02, 20);
+            SimpleDataBase<int> BasisData = new SimpleDataBase<int>();
+            BasisData.AddNewData(13);
+            BasisData.AddNewData(02);
+            BasisData.AddNewData(20);
+            BasisData.PrintAllData();
         }
     }
 }
---
+++ SimpleDataBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mod4
+{
+    public class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T dataBaru)
+        {
+            this.storedData.Add(dataBaru);
+            this.inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            foreach (T data_generic in this.storedData)
+            {
+                foreach(DateTime waktu in this.inputDates)
+                {
+                    Console.WriteLine("Data Yang Berisi : " + data_generic + " disimpan pada waktu : " + waktu);
+                }
+            }
+            
+        }
+            
+
+    }
+}
